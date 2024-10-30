b91488393f4ae4e4f9dc21a0b6db3d1859c0024c
John Agwu <agujohnifeanyi69@gmail.com>
2024-10-30 19:14:40 +0000

    added vpc and subnet since my account doesnt have default vpc

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ provider "aws" {
 resource "aws_instance" "ec-create" {
   ami           = "ami-0b4c7755cdf0d9219"
   instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnet.id
 
   tags = {
     Name = "Node-Created-By-GitAction"
@@ -16,5 +17,23 @@ terraform {
     bucket = "jonag-terraform-store"
     key    = "cicd-project/terraform.tfstate"
     region = "eu-west-2"
+  }
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "192.168.0.0/16"
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "192.168.1.0/24"
+  availability_zone = "eu-west-2a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "main-subnet"
   }
 }
