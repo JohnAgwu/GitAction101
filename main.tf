9bbae6a87bce450c4074b2384f4c1f9f4d236d91
John Agwu <agujohnifeanyi69@gmail.com>
2024-10-30 21:46:24 +0000

    added public rt

--- main.tf
+++ main.tf
@@ -39,3 +39,32 @@ resource "aws_subnet" "public_subnet" {
     Name = "main-subnet"
   }
 }
+
+# Create an Internet Gateway
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "project-igw"
+  }
+}
+
+
+# Route Table for the Public Subnet
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+# Associate the Route Table with the Public Subnet
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public.id
+}
